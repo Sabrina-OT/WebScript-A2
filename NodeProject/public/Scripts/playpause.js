b7ac441b66589d1f66df9eb0b7c2a43b91e10530
Sabrina-OT <sabrina.skoric@ontariotechu.net>
2023-11-01 10:24:47 -0400

    delete error

--- NodeProject/public/Scripts/playpause.js
+++
@@ -1,7 +0,0 @@
-//go to html file, get the element video, store in var myVideo
-var myVideo=document.getElementById("video"); 
-function playPause() 
-{ 
-    if (myVideo.paused) myVideo.play(); 
-    else myVideo.pause(); 
-} 
